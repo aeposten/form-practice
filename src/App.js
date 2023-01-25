89e0e8cb19da83e83d1d8fdf046892424f9d2fd5
Amy <86963506+aeposten@users.noreply.github.com>
2023-01-25 09:26:06 -0800

    Added basic styling and state handling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Form from "./components/Form";
 function App() {
   return (
     <div className="App">
+      <h1>React Form Practice</h1>
       <Form />
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,38 @@
+import { useState } from "react";
+
 function Form() {
-    return (
-        <form>
-            Form
-        </form>
-    )
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+  return (
+    <form>
+      <label htmlFor="first-name">First Name:
+        <input
+          type="text"
+          name="firstName"
+          value={formData.firstName}
+          id="first-name"
+        />
+      </label>
+      <label htmlFor="last-name">Last Name:
+        <input
+          type="text"
+          name="lastName"
+          value={formData.lastName}
+          id="last-name"
+        />
+      </label>
+      <label htmlFor="email">Email Address:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          id="email"
+        />
+      </label>
+    </form>
+  );
 }
 export default Form;
